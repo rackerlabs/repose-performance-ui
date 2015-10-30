9e4fd7f97406a681c71d3eed9cb9b7c28d4746a0
David Kowis <dkowis@shlrm.org>
2015-10-29 22:17:25 -0500

    Trying to make hopefully useful notes?

--- app/controllers/SubApps.scala
+++ app/controllers/SubApps.scala
@@ -31,8 +31,7 @@ class SubApps extends Controller {
 
   //https://github.com/rackerlabs/repose-performance-ui/blob/master/README.md#get-appresultsresultid
   def resultDetails(appId: String, resultId: String) = Action {
-    //For the given sub App, ask redits for each of the test liststs?
-
+    //For the given sub App, ask redis for each of the test liststs?
 
     val testTypes = LameDB.listTestTypes()
     //For each of these test types, get the status of the test that was ran against this sub app
@@ -44,6 +43,30 @@ class SubApps extends Controller {
 
     //Now collect all the statuses for each type
     //TODO: all this would also have to come from some redis :(
+
+    /*
+    To do this, we'll have to have some results json, or figure out all the possible things from the ruby code.
+    Unfortunately, the ruby code merges many objects together and deals with them all as one giant hash :( This
+    makes it super hard to figure out what's from what :(
+
+    You pars the json test results from line 17 of model/results.rb, aswell as the data result.
+
+    Then you grab the "test" field, which has something, and you merge that with something that came in as part of the test_list. I think
+    the test list is another JSON structure from redis.
+
+    Then, there is two different ways to compile the summary results from either Gatling or JMeter. Not yet sure how that gets set.
+
+    This mutates the state of the same test_hash we were building earlier :( Ugh. I have literally no idea what's going on right now.
+    I think the compile summary results methods are just a way to collect details out of the plain text results from gatling. Some
+    regexps exist to try to pull that data out. That would be a standalone module on its own. Something to parse the gatling result
+    data from the redis stored data.
+
+    Actually now I'm not even sure where it's getting the data, it's making an HTTP call to "fs_ip" AH, it's a file storage.
+    Wow. This needs to be completely replaced with an agent running on the systems under test, or using an SSH connection
+    to orchestrate the tests so we can collect those file bits and post them to the server, process them once, rather than
+    every time (I think they're processed every time)
+
+     */
 
     Ok("NOT YET DONE")
   }
